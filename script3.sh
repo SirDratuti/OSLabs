2c4246934bebe7543f1e7dd0bb24bab582d4bfc6
Andrey Belitsckiy <60936060+SirDratuti@users.noreply.github.com>
2020-11-09 14:12:51 +0300

    Delete script3.sh

--- script3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-touch tmp
-ps -e -o start,pid,command --no-header > tmp
-cat tmp | grep -v "script3.sh" |  grep -v "ps -e -o start,pid,command --no-header" | tail -n 1 | awk '{print $2,$3}'
